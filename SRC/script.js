dade143689220f5f34afea050d10144208d7b15d
plug_keith <keith.mwai@student.moringaschool.com>
2024-07-23 20:13:44 +0300

    first commit

---
+++ SRC/script.js
@@ -0,0 +1,9 @@
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+// const monsterList = document.getElementById('list');
+// const monsterForm = document.getElementById('M_form');
+// })
+
+// // fetch and display the 50
+// fetch
